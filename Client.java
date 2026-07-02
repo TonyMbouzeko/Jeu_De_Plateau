c8525da3fa5a36770511017295506feef9fed07d
Tony Mbouzeko <tony-bonnel.mbouzeko-mouafo.1@ens.etsmtl.ca>
2026-07-02 14:24:43 -0400

    Petite Erreur au niveau de cmd == 4 ¸ ça boucle infinimment.

--- Client.java
+++ Client.java
@@ -17,10 +17,13 @@ class Client {
 
 		
 		int[][] board = new int[13][13];
+		Move dernierCoupEnvoye = null;
+		String dernierCoupEnvoyeServeur = null;
 
 		try {
 			Client client = new Client();
 			Board b = new Board();
+			IntelligenceArtificielle iA = new IntelligenceArtificielle();
 			BufferedInputStream input = client.input;
 			BufferedOutputStream output = client.output;
 			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
@@ -43,8 +46,8 @@ class Client {
 					b.loadFromServer(boardValues);
 
 					System.out.println("Nouvelle partie! Vous jouer rouge, entrez votre premier coup : ");
-					String move = null;
-					move = console.readLine();
+					Move mouvement = iA.jouer(b, b.GetCurrentPlayer());
+					String move = formatMoveToServer(mouvement);
 					output.write(move.getBytes(), 0, move.length());
 					output.flush();
 				}
@@ -74,19 +77,38 @@ class Client {
 					input.read(aBuffer, 0, size);
 
 					String s = new String(aBuffer);
+					Mark adversaire;
+					if (b.GetCurrentPlayer() == Mark.NOIR){
+						adversaire = Mark.ROUGE;
+					}else{
+						adversaire = Mark.NOIR;
+					}
+
+					Move coupAdverse = formatServerToMove(s);
+					b.play(coupAdverse,  adversaire);
 					System.out.println("Dernier coup :" + s);
 					System.out.println("Entrez votre coup : ");
-					String move = null;
-					move = console.readLine();
+					Move mouvement = iA.jouer(b, b.GetCurrentPlayer());
+
+					b.play(mouvement, b.GetCurrentPlayer());
+
+					dernierCoupEnvoye = mouvement;
+					dernierCoupEnvoyeServeur = formatMoveToServer(mouvement);
+
+					System.out.println("Coup envoyé au serveur : " + dernierCoupEnvoyeServeur);
+
+					String move = formatMoveToServer(mouvement);
 					output.write(move.getBytes(), 0, move.length());
 					output.flush();
 
 				}
 				// Le dernier coup est invalide
 				if (cmd == '4') {
-					System.out.println("Coup invalide, entrez un nouveau coup : ");
-					String move = null;
-					move = console.readLine();
+					System.out.println("Coup invalide, entrez un nouveau coup :");
+					System.out.println("Dernière chaîne envoyée : " + dernierCoupEnvoyeServeur);
+				
+					Move mouvement = iA.jouer(b, b.GetCurrentPlayer());
+					String move = formatMoveToServer(mouvement);
 					output.write(move.getBytes(), 0, move.length());
 					output.flush();
 
@@ -134,5 +156,26 @@ class Client {
     // Format attendu par le serveur : A13-B13
     return "" + colDep + ligDep + "-" + colArr + ligArr;
 }
+
+	public static Move formatServerToMove(String s){
+		s = s.trim();
+		s = s.replaceAll("\\s+","");
+		String[] parties = s.split("-");
+		if (parties.length != 2){
+			return null;
+		}
+
+		char depart = parties[0].charAt(0);
+		int coldepart = depart -'A';
+
+		int ligneDepart = 13 - Integer.parseInt(parties[0].substring(1));
+
+		char arrivee = parties[1].charAt(0);
+		int colArrive = arrivee - 'A';
+
+		int ligneArrive = 13 - Integer.parseInt(parties[1].substring(1));
+
+		return new Move( ligneDepart, coldepart, ligneArrive, colArrive);
+	}
 
 }
